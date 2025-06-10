74da99bb097071209037dcee3a637c9fd40ced42
naoto-nakamura-ac <naoto_nakamura_ac@mail.toyota.co.jp>
2025-06-10 11:29:44 +0900

    Created Entity@2

--- src/main/kotlin/com/example/kajikashiApp/entity/Task.kt
+++ src/main/kotlin/com/example/kajikashiApp/entity/Task.kt
@@ -3,7 +3,6 @@ package com.example.kajikashiApp.entity
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.EntityListeners
-import jakarta.persistence.FetchType
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id

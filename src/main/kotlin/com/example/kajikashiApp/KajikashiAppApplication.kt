1aca951745e3eef453b69889c80d156d8f95532f
naoto-nakamura-ac <naoto_nakamura_ac@mail.toyota.co.jp>
2025-06-11 01:06:47 +0900

    Add annotasion

--- src/main/kotlin/com/example/kajikashiApp/KajikashiAppApplication.kt
+++ src/main/kotlin/com/example/kajikashiApp/KajikashiAppApplication.kt
@@ -2,8 +2,10 @@ package com.example.kajikashiApp
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Configuration
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
+@Configuration
 @EnableJpaAuditing
 @SpringBootApplication
 class KajikashiAppApplication

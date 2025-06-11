bb2141f7a76a1595a591499378f1fd00b67bde76
naoto-nakamura-ac <naoto_nakamura_ac@mail.toyota.co.jp>
2025-06-11 16:34:55 +0900

    Create user method

--- src/main/kotlin/com/example/kajikashiApp/repository/UserRepository.kt
+++ src/main/kotlin/com/example/kajikashiApp/repository/UserRepository.kt
@@ -1,8 +1,10 @@
 package com.example.kajikashiApp.repository
 
+import com.example.kajikashiApp.entity.Family
 import com.example.kajikashiApp.entity.User
 import org.springframework.data.repository.CrudRepository
 
 interface UserRepository: CrudRepository<User, Long> {
     fun findByEmail(email: String): User?
+    fun findAllByFamilyId(familyId: Long): List<User>?
 }
